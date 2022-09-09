5e54b1b2a28df44904a4629ffdfdd5a4b4ba143e
cmyui <cmyuiosu@gmail.com>
2022-09-09 02:16:55 -0400

    feat: the first unit test! account signup

--- mount/tests/usecases/test_accounts.py
+++ mount/tests/usecases/test_accounts.py
@@ -1,5 +1,6 @@
 import pytest
 from app.common.context import Context
+from app.common.errors import ServiceError
 from app.usecases import accounts
 
 # https://docs.pytest.org/en/7.1.x/reference/reference.html#globalvar-pytestmark
@@ -7,4 +8,27 @@ pytestmark = pytest.mark.asyncio
 
 
 async def test_should_pass(ctx: Context):
-    assert True
+    assert ctx is not None
+
+
+async def test_should_signup(ctx: Context):
+    username = "cmyui"
+    password = "lol123"
+    email_address = "cmyuiosu@gmail.com"
+    country = "CA"
+    account = await accounts.signup(ctx,
+                                    username=username,
+                                    password=password,
+                                    email_address=email_address,
+                                    country=country)
+    assert not isinstance(account, ServiceError)
+
+    assert account["rec_id"] is not None
+    assert account["account_id"] is not None
+    assert account["username"] == username
+    assert account["email_address"] == email_address
+    assert account["country"] == country
+    assert account["created_at"] is not None
+    assert account["updated_at"] is not None
+
+    # TODO: should we check the credentials were created correctly?
