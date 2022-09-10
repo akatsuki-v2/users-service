547a7c6d84ff50b22c4a1b0be295ffd4472bf105
cmyui <cmyuiosu@gmail.com>
2022-09-10 01:28:06 -0400

    perf: use exec to execute pytest from run-tests script

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,6 +2,7 @@
 set -eo pipefail
 
 FULL_TEST_DB_NAME="${WRITE_DB_NAME}_test"
+echo -e "\x1b[;93mRunning tests on '${FULL_TEST_DB_NAME}' database\x1b[m"
 
 echo "Recreating database.."
 echo "Dropping database ${FULL_TEST_DB_NAME}.."
@@ -27,13 +28,13 @@ echo "Running database seeds.."
 # make sure we're not running cache
 find . -name "*.pyc" -delete
 export PYTHONDONTWRITEBYTECODE=1
-export PYTHONPATH=$PYTHONPATH:/srv/root
 
+# use /srv/root as home
+export PYTHONPATH=$PYTHONPATH:/srv/root
 cd /srv/root
 
-pytest \
+exec pytest tests/ \
     --cov-config=tests/coverage.ini \
     --cov=app \
     --cov-report=term \
-    --cov-report=html:tests/htmlcov \
-    tests/
+    --cov-report=html:tests/htmlcov
