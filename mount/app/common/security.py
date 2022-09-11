fd49a533f8ee1b583fdf73938b2b6db6a8e7bdc1
cmyui <cmyuiosu@gmail.com>
2022-09-11 18:36:12 -0400

    chore: make password hashing compatible with osu! (hash with md5 before bcrypt)

--- mount/app/common/security.py
+++ mount/app/common/security.py
@@ -1,4 +1,5 @@
 import asyncio
+import hashlib
 
 import bcrypt
 
@@ -16,6 +17,10 @@ async def check_password(password: str, hashed: str) -> bool:
 
 async def hash_password(password: str) -> str:
     loop = asyncio.get_event_loop()
+
+    # NOTE: osu! hashes passwords with md5 before passing them to bcrypt.
+    password = hashlib.md5(password.encode('utf-8')).hexdigest()
+
     return (await loop.run_in_executor(DEFAULT_EXECUTOR,
                                        bcrypt.hashpw,
                                        password.encode('utf-8'),
