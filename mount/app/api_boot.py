e3f996db144d012039d1dfd2e282bb8f5c2777e2
cmyui <cmyuiosu@gmail.com>
2022-09-12 01:59:33 -0400

    feat: configure log level at api boot

--- mount/app/api_boot.py
+++ mount/app/api_boot.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
 from app.api.rest import init_api
+from app.common import logging
+from app.common import settings
+logging.init_logging(log_level=settings.LOG_LEVEL)
 
 api = init_api()
