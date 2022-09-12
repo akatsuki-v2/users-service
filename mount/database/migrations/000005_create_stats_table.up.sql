f0c7de500ec2d447880fd9df19fcdc7d24e9975d
cmyui <cmyuiosu@gmail.com>
2022-09-11 23:54:03 -0400

    feat: database migrations for accounts stats

---
+++ mount/database/migrations/000005_create_stats_table.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE accounts_stats (
+    account_id INTEGER NOT NULL,
+    game_mode SMALLINT NOT NULL,
+    total_score BIGINT NOT NULL,
+    ranked_score BIGINT NOT NULL,
+    performance INT NOT NULL,
+    play_count INT NOT NULL,
+    play_time INT NOT NULL,
+    accuracy REAL NOT NULL,
+    max_combo INT NOT NULL,
+    total_hits INT NOT NULL,
+    replay_views INT NOT NULL,
+    xh_count INT NOT NULL,
+    x_count INT NOT NULL,
+    sh_count INT NOT NULL,
+    s_count INT NOT NULL,
+    a_count INT NOT NULL,
+    PRIMARY KEY (account_id, game_mode)
+);
