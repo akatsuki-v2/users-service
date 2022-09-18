3896aa90da98fc8d2f14b34ab1d1b31be3269460
cmyui <cmyuiosu@gmail.com>
2022-09-18 05:52:31 -0400

    feat: implement correct response models for success/error cases

--- mount/app/api/rest/v1/accounts.py
+++ mount/app/api/rest/v1/accounts.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from app.api.rest.context import RequestContext
 from app.common import responses
 from app.common.errors import ServiceError
+from app.common.responses import Success
 from app.models import Status
 from app.models.accounts import Account
 from app.models.accounts import AccountUpdate
@@ -15,7 +16,7 @@ router = APIRouter()
 
 
 # https://osuakatsuki.atlassian.net/browse/V2-10
-@router.post("/v1/accounts", response_model=Account)
+@router.post("/v1/accounts", response_model=Success[Account])
 async def sign_up(args: SignupForm, ctx: RequestContext = Depends()):
     data = await accounts.sign_up(ctx,
                                   args.username,
@@ -30,7 +31,7 @@ async def sign_up(args: SignupForm, ctx: RequestContext = Depends()):
 
 
 # https://osuakatsuki.atlassian.net/browse/V2-33
-@router.get("/v1/accounts", response_model=list[Account])
+@router.get("/v1/accounts", response_model=Success[list[Account]])
 async def get_accounts(country: str | None = None,
                        status: Status | None = Status.ACTIVE,
                        ctx: RequestContext = Depends()):
@@ -43,7 +44,7 @@ async def get_accounts(country: str | None = None,
 
 
 # https://osuakatsuki.atlassian.net/browse/V2-58
-@router.get("/v1/accounts/{account_id}", response_model=Account)
+@router.get("/v1/accounts/{account_id}", response_model=Success[Account])
 async def get_account(account_id: int,
                       status: Status | None = None,
                       ctx: RequestContext = Depends()):
@@ -56,7 +57,7 @@ async def get_account(account_id: int,
 
 
 # https://osuakatsuki.atlassian.net/browse/V2-59
-@router.patch("/v1/accounts/{account_id}", response_model=Account)
+@router.patch("/v1/accounts/{account_id}", response_model=Success[Account])
 async def partial_update_account(account_id: int, args: AccountUpdate,
                                  ctx: RequestContext = Depends()):
     data = await accounts.partial_update(ctx, account_id, **args.dict())
@@ -68,7 +69,7 @@ async def partial_update_account(account_id: int, args: AccountUpdate,
 
 
 # https://osuakatsuki.atlassian.net/browse/V2-44
-@router.delete("/v1/accounts/{account_id}", response_model=Account)
+@router.delete("/v1/accounts/{account_id}", response_model=Success[Account])
 async def delete_account(account_id: int, ctx: RequestContext = Depends()):
     data = await accounts.delete(ctx, account_id)
     if isinstance(data, ServiceError):
