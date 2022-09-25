9399cb06a5f6570a9551cb0af5e0e672faf22f8d
cmyui <cmyuiosu@gmail.com>
2022-09-25 19:59:24 -0400

    feat: add api endpoints for queued packets

--- mount/app/api/rest/v1/__init__.py
+++ mount/app/api/rest/v1/__init__.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 
 from . import accounts
 from . import presences
+from . import queued_packets
 from . import sessions
 from . import stats
 
@@ -14,3 +15,4 @@ router.include_router(accounts.router, tags=["accounts"])
 router.include_router(presences.router, tags=["presences"])
 router.include_router(sessions.router, tags=["sessions"])
 router.include_router(stats.router, tags=["stats"])
+router.include_router(queued_packets.router, tags=["queued packets"])
